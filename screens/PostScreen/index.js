6f53988105a757e3894181225596cd751cf64bb6
surajpandeyui <suraj.p@collegepass.org>
2023-03-10 14:36:51 +0530

    modal

--- screens/PostScreen/index.js
+++ screens/PostScreen/index.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useEffect, useState } from 'react'
-import { Container, Row, Col, Button, Form } from 'react-bootstrap'
+import { Container, Row, Col, Button, Form, Modal } from 'react-bootstrap'
 import styles from '../BlogScreen/blog.module.scss'
 import Image from 'next/image'
 import axios from 'axios'
@@ -14,6 +14,12 @@ import moment from 'moment'
 import Link from 'next/link'
 
 const index = ({ id }) => {
+
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+
   const [post, setPost] = useState()
   const [extraPosts, setExtraPosts] = useState([])
 
@@ -332,12 +338,37 @@ const index = ({ id }) => {
                         color: '#545454',
                         fontSize: '14px'
                       }}>Hi, This is a dummy comment here..</p>
-                      <a href="" className={styles.commentReply}>Reply</a>
+                      <p className={styles.commentReply} onClick={handleShow}>Reply</p>
                     </Col>
                   </Row>
                 </Col>
               </Row>
 
+              <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                  <Modal.Title>Advisor Login</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                  <Row>
+                    <Col>
+                      <Form>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                          <Form.Label>Login</Form.Label>
+                          <Form.Control type="email" placeholder="Enter email" />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                          <Form.Label>Password</Form.Label>
+                          <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                          Submit
+                        </Button>
+                      </Form>
+                    </Col>
+                  </Row>
+                </Modal.Body>
+              </Modal>
               <Row>
                 <Col className="pt-5 pb-5">
                   {!user && (
