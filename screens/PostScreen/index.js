c64a3879461bb50a7c6e7361f987dde7ee14d78c
surajpandeyui <suraj.p@collegepass.org>
2023-04-06 14:26:08 +0530

    tooltip

--- screens/PostScreen/index.js
+++ screens/PostScreen/index.js
@@ -467,7 +467,7 @@ const index = ({ id }) => {
                       <h5 style={{
                             fontWeight: '600',
                             color: '#ffffff',
-                            paddingTop: '25px',
+                            paddingTop: '60px',
                             letterSpacing: '0.03rem'
                       }}>Found this article useful? Share it!</h5>
                     </Col>
